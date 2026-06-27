951629ea30380d4a7080251c090893a239bb37f5
Matteomio16 <Matteomio16@users.noreply.github.com>
2026-06-27 19:04:05 +0100

    Remove temporary test files

--- _test_orig_compile.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const js = fs.readFileSync('_test_orig.js', 'utf8');
-try {
-  new Function('DCLogic', 'StreamableLogic', 'React', js + '\nreturn Component;');
-  console.log('ORIGINAL: OK');
-} catch(e) {
-  console.log('ORIGINAL ERROR:', e.message);
-}
